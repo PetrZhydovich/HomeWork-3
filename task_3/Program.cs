9f214ef6bce45ef57c5327dea5939ea7b20561da
Petr <povar_cr@mail.ru>
2022-02-13 19:18:34 +0300

    chenge task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -5,7 +5,6 @@ double Coards(double arg)
     return arg;
 }
 
-// Получаем значения для координат из рандома
 double num = 0;
 double x1 = Coards(num);
 double x2 = Coards(num);
@@ -13,9 +12,6 @@ double y1 = Coards(num);
 double y2 = Coards(num);
 double z1 = Coards(num);
 double z2 = Coards(num);
-
-// Сравниваем какая из координат больше, для формулы вычисления расстояния
-// присваиваем значения каждой координаты
 
 if (x1 > x2)
 {
@@ -38,17 +34,13 @@ if (z1 > z2)
     z2 = temp;
 }
 
-// Выводим правильное расположение координат 2d = 3d, где z1 == 0 и z2 == 0
 Console.WriteLine($"Координата в пространстве 2d: x1={x1}, x2={x2}, y1={y1}, y2={y2}");
 
-// Выводим правильное расположение координат 3d
 Console.WriteLine($"Координата в пространстве 3d: x1={x1}, x2={x2}, y1={y1}, y2={y2}, z1={z1}, z2={z2}");
 
-// вычисляем корень из суммы длин 2d
 double distance2D = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
 Console.WriteLine($"Расстояние между точками в пространстве 2d = {distance2D}");
 
-// вычисляем корень из суммы длин 3d
 double distance3D = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)) + ((z2 - z1) * (z2 - z1)));
 Console.WriteLine($"Расстояние между точками в пространстве 3d = {distance3D}");
 
