889804c59f6a7313147da5e6fe456bffdc5d1fdf
Petr <povar_cr@mail.ru>
2022-02-13 18:00:06 +0300

    add Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задать номер четверти, показать диапазоны для возможных координат
+
+int GetRandomNumber()
+{
+    int randomNumb = new Random().Next(1,5);
+    return randomNumb;
+}
+void Coordinati(int chet) 
+{
+    if(chet == 1)
+        Console.WriteLine("для этой четверти возможны координаты Из диапазона x>0,y>0");
+    else if(chet == 2)
+        Console.WriteLine ("для этой четверти возможны координаты Из диапазона x<0,y>0");
+    else if(chet == 3)
+        Console.WriteLine ("для этой четверти возможны координаты Из диапазона x<0,y<0");
+    else if(chet == 4)
+        Console.WriteLine ("для этой четверти возможны координаты Из диапазона x>0,y<0"); 
+
+}
+
+int chetvert = GetRandomNumber();
+Console.WriteLine($"номер четверти: {chetvert}");
+Coordinati(chetvert); 
